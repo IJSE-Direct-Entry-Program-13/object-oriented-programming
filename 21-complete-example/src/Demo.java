517a8d140d8003eebcf413fc8f78ca46e7211280
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-11-18 17:32:00 +0530

    Add complete example output

--- 21-complete-example/src/Demo.java
+++ 21-complete-example/src/Demo.java
@@ -10,24 +10,29 @@ public class Demo {
 
 class Parent {
     static int a = 20;
+
     static {
         System.out.println("Parent class is being initialized");
     }
+
     int b = 20;
+
+    {
+        System.out.println("Parent Instance Initializer");
+        b = a;
+        System.out.println("b=" + b);
+    }
+
     public Parent() {
         super();
         System.out.println("Parent()");
         System.out.println("b=" + b);
     }
+
     public Parent(int b) {
         this();
         this.b = b;
         System.out.println("Parent(int)");
-        System.out.println("b=" + b);
-    }
-    {
-        System.out.println("Parent Instance Initializer");
-        b = a;
         System.out.println("b=" + b);
     }
 }
@@ -37,22 +42,26 @@ class Child extends Parent {
     static {
         System.out.println("Child class is being initialized");
     }
+
+
     {
         System.out.println("Child Instance Initializer 1");
         c = b;
     }
     int c = -30;
+    {
+        System.out.println("Child Instance Initializer 2");
+        System.out.println("c=" + c);
+        c = a;
+    }
+
     public Child() {
         this(20, a);
         System.out.println("Child()");
         System.out.println("a=" + a);
         System.out.println("c=" + c);
     }
-    {
-        System.out.println("Child Instance Initializer 2");
-        System.out.println("c=" + c);
-        c = a;
-    }
+
     public Child(int a, int b) {
         super(b);
         System.out.println("Child(int)");
