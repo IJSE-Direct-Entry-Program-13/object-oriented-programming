29ffe122ca987828b358aabb8affb31d581d636a
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-11-26 18:30:33 +0530

    Add database script

---
+++ 50-abstraction-practical-example/db-script.sql
@@ -0,0 +1,12 @@
+CREATE TABLE customer(
+                         id VARCHAR(10) PRIMARY KEY ,
+                         name VARCHAR(100) NOT NULL,
+                         address VARCHAR(400) NOT NULL
+);
+
+TABLE customer;
+
+INSERT INTO customer (id, name, address)
+VALUES ('C003', 'Upul', 'Maharagama');
+
+DELETE FROM customer WHERE id='C001';
