ce01ddaf2c47c12d64a991a693be72d2aa3fe5b6
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-11-26 18:30:48 +0530

    Fix the format

--- 50-abstraction-practical-example/db-script.sql
+++ 50-abstraction-practical-example/db-script.sql
@@ -1,7 +1,8 @@
-CREATE TABLE customer(
-                         id VARCHAR(10) PRIMARY KEY ,
-                         name VARCHAR(100) NOT NULL,
-                         address VARCHAR(400) NOT NULL
+CREATE TABLE customer
+(
+    id      VARCHAR(10) PRIMARY KEY,
+    name    VARCHAR(100) NOT NULL,
+    address VARCHAR(400) NOT NULL
 );
 
 TABLE customer;
@@ -9,4 +10,6 @@ TABLE customer;
 INSERT INTO customer (id, name, address)
 VALUES ('C003', 'Upul', 'Maharagama');
 
-DELETE FROM customer WHERE id='C001';
+DELETE
+FROM customer
+WHERE id = 'C001';
