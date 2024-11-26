b589721d937ec21274d0ad70fa9a5a27c0a411c6
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-11-26 17:44:44 +0530

    Add file data access

--- 50-abstraction-practical-example/src/main/java/lk/ijse/dep13/oop/abstraction/data/FileDataAccess.java
+++ 50-abstraction-practical-example/src/main/java/lk/ijse/dep13/oop/abstraction/data/FileDataAccess.java
@@ -1,14 +1,36 @@
 package lk.ijse.dep13.oop.abstraction.data;
 
-import lk.ijse.dep13.oop.abstraction.tm.Customer;
-
 import java.io.*;
 import java.util.ArrayList;
 
-public class FileDataAccess implements DataAccess{
+public class FileDataAccess implements DataAccess {
 
-    private final File file = new File("db.dep");
+    private final File dbFile = new File("db.dep");
     private ArrayList<CustomerTo> customerList = new ArrayList<>();
+
+    {
+        readCustomersFromFile();
+    }
+
+    public boolean writeCustomersToFile(ArrayList<CustomerTo> customerList) {
+        try (var fos = new FileOutputStream(dbFile);
+             var oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(customerList);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private void readCustomersFromFile() {
+        try (var fis = new FileInputStream(dbFile);
+             var ois = new ObjectInputStream(fis)) {
+            customerList = (ArrayList<CustomerTo>) ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean saveCustomer(CustomerTo customer) {
@@ -18,10 +40,10 @@ public class FileDataAccess implements DataAccess{
         }
         ArrayList<CustomerTo> updatedCustomerList = new ArrayList<>(customerList);
         updatedCustomerList.add(customer);
-        if (writeCustomersToFile(updatedCustomerList)){
+        if (writeCustomersToFile(updatedCustomerList)) {
             customerList = updatedCustomerList;
             return true;
-        }else{
+        } else {
             return false;
         }
     }
@@ -33,9 +55,10 @@ public class FileDataAccess implements DataAccess{
             if (c.id().equals(id)) {
                 ArrayList<CustomerTo> updatedCustomerList = new ArrayList<>(customerList);
                 updatedCustomerList.remove(c);
-                if (writeCustomersToFile(updatedCustomerList)){
+                if (writeCustomersToFile(updatedCustomerList)) {
                     customerList = updatedCustomerList;
-                }else{
+                    return true;
+                } else {
                     return false;
                 }
             }
@@ -43,18 +66,8 @@ public class FileDataAccess implements DataAccess{
         return false;
     }
 
-    private boolean writeCustomersToFile(ArrayList<CustomerTo> customerList) {
-        try(var oos = new ObjectOutputStream(new FileOutputStream(file))){
-            oos.writeObject(customerList);
-            return true;
-        }catch (Exception e){
-            e.printStackTrace();
-            return false;
-        }
-    }
-
     @Override
     public ArrayList<CustomerTo> findAllCustomers() {
-        return null;
+        return customerList;
     }
 }
