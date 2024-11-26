fd8113416c98eeaedbfd9a1999bb3e81d9755861
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-11-26 12:56:28 +0530

    Update the controller with fields and apis

--- 50-abstraction-practical-example/src/main/java/lk/ijse/dep13/oop/abstraction/controller/MainSceneController.java
+++ 50-abstraction-practical-example/src/main/java/lk/ijse/dep13/oop/abstraction/controller/MainSceneController.java
@@ -1,4 +1,30 @@
 package lk.ijse.dep13.oop.abstraction.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class MainSceneController {
+
+    public Button btnNewCustomer;
+    public Button btnRemove;
+    public Button btnSave;
+    public TableView<?> tblCustomers;
+    public TextField txtAddress;
+    public TextField txtId;
+    public TextField txtName;
+
+    public void btnNewCustomerOnAction(ActionEvent event) {
+
+    }
+
+    public void btnRemoveOnAction(ActionEvent event) {
+
+    }
+
+    public void btnSaveOnAction(ActionEvent event) {
+
+    }
+
 }
