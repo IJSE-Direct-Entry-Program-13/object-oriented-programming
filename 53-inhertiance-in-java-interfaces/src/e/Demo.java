d41999445590be9927d54e885938d9b050df9866
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2024-11-29 11:16:35 +0530

    Add demo

--- 53-inhertiance-in-java-interfaces/src/e/Demo.java
+++ 53-inhertiance-in-java-interfaces/src/e/Demo.java
@@ -3,8 +3,8 @@ package e;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println(SubInterface.VALUE);
-        SubInterface.print();
+        //System.out.println(SubInterface.VALUE);
+        //SubInterface.print();
     }
 }
 
